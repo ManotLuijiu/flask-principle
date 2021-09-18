a7ba5e327c8b2ddb174981212ea9937327fb42af
Manot Luijiu <manot.luijiu@gmail.com>
2021-09-18 13:10:02 +0700

    Adding app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+# Call flask framework
+from flask import Flask
+
+# Initialize the application
+app = Flask(__name__, instance_relative=True)
+
+# Load the views
+from app import views
+
+# Load the configuration file
+app.config.from_object('config')
